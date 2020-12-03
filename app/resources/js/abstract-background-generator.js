0b5fd922bc912d7cee8a0fd7d6a4d66995e0a71e
DimovDimo <d.dimov1996@gmail.com>
2020-12-03 11:50:34 +0200

    added speed, width & height

--- app/resources/js/abstract-background-generator.js
+++ app/resources/js/abstract-background-generator.js
@@ -1,2 +1,10 @@
+let speed = 1000;
+
+let width = 1000;
+let height = 1000;
+
 let canvas = document.getElementById("background-result");
 let context = canvas.getContext('2d');
+
+canvas.width = width;
+canvas.height = height;
