a85e81fe15f48502c60777684b534e20b5e375db
DimovDimo <d.dimov1996@gmail.com>
2020-12-04 10:55:34 +0200

    added click for generator

--- app/resources/js/abstract-background-generator.js
+++ app/resources/js/abstract-background-generator.js
@@ -8,3 +8,10 @@ let context = canvas.getContext('2d');
 
 canvas.width = width;
 canvas.height = height;
+
+canvas.addEventListener("click", generator);
+generator();
+
+function generator() {
+    //TODO
+}
